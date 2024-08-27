88601faeb7b0d949e3dbb8cb1b95f480276cc9ca
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-08-27 11:15:26 +0100

    Update question.js

--- question.js
+++ question.js
@@ -2,112 +2,57 @@
 let questions = [
     {
         numb: 1,
-        question: "What is the chemical symbol for water?",
-        answer: "b.)  H2O",
+        question: "How do you create a function in JavaScript?",
+        answer: "a.)  function myFunction() {}",
         options: [
-            "a.)  NaCl",
-            "b.)  H2O",
-            "c.)  CO2",
-            "d.)  O2"
+            "a.)  function myFunction() {}",
+            "b.)  def myFunction() {}",
+            "c.)  function: myFunction() {}",
+            "d.)  create myFunction() {}"
         ]
     },
     {
         numb: 2,
-        question: " What is the largest ocean on Earth?",
-        answer: "a.)  Parcific Ocean",
+        question: "What will the following code return: Boolean(10 > 9)?",
+        answer: "a.)  true",
         options: [
-            "a.)  Parcific Ocean",
-            "b.)  Arctic Ocean",
-            "c.)  Atlantic Ocean",
-            "d.)  Indian Ocean"
+            "a.)  true",
+            "b.)  false",
+            "c.)  NaN",
+            "d.)  null"
         ]
     },
     {
         numb: 3,
-        question: "What is the largest mammal in the world?",
-        answer: "c.)  Blue Whale",
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "a.)  alert('Hello World');",
         options: [
-            "a.)  Elephant",
-            "b.)  Great White Shark",
-            "c.)  Blue Whale",
-            "d.)  Giraffe"
+            "a.)  alert('Hello World');",
+            "b.)  msg('Hello World');",
+            "c.)  alertBox('Hello World');",
+            "d.)  msgBox('Hello World');"
         ]
     },
     {
         numb: 4,
-        question: "How many continents are there in the world?",
-        answer: "c.)  Seven",
+        question: "Which of the following is a correct way to declare a variable in JavaScript?",
+        answer: "a.)  let myVar;",
         options: [
-            "a.)  Five",
-            "b.)  Six",
-            "c.)  Seven",
-            "d.)  Eight"
+            "a.)  let myVar;",
+            "b.)  variable myVar;",
+            "c.)  v myVar;",
+            "d.)  varr myVar;"
         ]
     },
     {
         numb: 5,
-        question: "What is the hardest natural substance on Earth?",
-        answer: "a.)  Diamomd",
+        question: "How can you detect the client's browser name?",
+        answer: "a.)  navigator.appName",
         options: [
-            "a.)  Diamond",
-            "b.)  Silver",
-            "c.)  Gold",
-            "d.)  Iron"
+            "a.)  navigator.appName",
+            "b.)  browser.name",
+            "c.)  client.navName",
+            "d.)  browser.appName"
         ]
     },
-    {
-        numb: 6,
-        question: "Who is known as the 'Father of Computers'?",
-        answer: "a.)  Charles Barbage",
-        options: [
-            "a) Charles Barbage"
-            "b) William Shakespeare"
-            "c) Mark Twain"
-            "d) Leo Tolstoy"
-        ]
-    },
-    {
-        numb: 7,
-        question: "Which element has the chemical symbol 'O'",
-        answer: "a.)  Oxygen",
-        options: [
-            "a.)  Oxygen",
-            "b.)  Gold",
-            "c.)  Iron",
-            "d.)  Silver"
-        ]
-    },
-    {
-        numb: 8,
-        question: "What is the smallest country in the world by land area?",
-        answer: "c) Vatican City",
-        options: [
-            "a) Monac"
-            "b) Nauru"
-            "c) Vatican City"
-            "d) San Marino"
-        ]
-    },
-    {
-        numb: 9,
-        question: "What is the tallest mountain in the world?",
-        answer: "c) Mount Everest",
-        options: [
-            "a) K2"
-            "b) Mount Kilimanjaro"  
-            "c) Mount Everest" 
-            "d) Mount Elbrus"
-        ]
-    },
-    {
-        numb: 10,
-        question: "Which gas do plants absorb from the atmosphere?",
-        answer: "c.)  Carbondioxide",
-        options: [
-            "a.)  Oxygen",
-            "b.)  Nitrogen",
-            "c.)  Carbondioxide",
-            "d.)  hydrogen"
-        ]
-    },
-];
+    ];
