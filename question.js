6a1e72ccea0dd692ca1017bf334b8d7620b19fa6
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-08-27 12:02:14 +0100

    Update question.js

--- question.js
+++ question.js
@@ -55,4 +55,59 @@ let questions = [
             "d.)  Iron"
         ]
     },
+       {
+        numb: 6,
+        question: "Who is known as the (father of computer) ",
+        answer: "a.)  Charles Barbage",
+        options: [
+            "a.)  Charles Barbage",
+            "b.)  William Shakespeare",
+            "c.)  Mark Twain",
+            "d.)  Leo Toistoy"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which Element has the chemical symbol (O) ",
+        answer: "b.)  Oxygen",
+        options: [
+            "a.)  Gold",
+            "b.)  Oxygen",
+            "c.)  Iron",
+            "d.)  Silver"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the smallest country in  the world by land area?",
+        answer: "d.)  Vatican  City",
+        options: [
+            "a.)  Monac",
+            "b.)  Nauru",
+            "c.)  San Marino",
+            "d.)  Vatican City"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the Tallest Mountain in  the world?",
+        answer: "c.)  Mount Everest",
+        options: [
+            "a.)  K2",
+            "b.)  Mount Kilimanjaro",
+            "c.)  Mount Everest",
+            "d.)  Mount Elbrus"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which gas do plant absorb from the  atmosphere?",
+        answer: "c.)  Carbondioxide",
+        options: [
+            "a.)  Oxygen",
+            "b.)  Nitrogen",
+            "c.)  carbondioxie",
+            "d.)  hydrogen"
+        ]
+    }, 
     ];
