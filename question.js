04ec2403949c93b4b3a975cc8086d8573ada11d0
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-08-27 08:52:48 +0100

    Create question.js

---
+++ question.js
@@ -0,0 +1,113 @@
+//An Array passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "How do you create a function in JavaScript?",
+        answer: "a.)  function myFunction() {}",
+        options: [
+            "a.)  function myFunction() {}",
+            "b.)  def myFunction() {}",
+            "c.)  function: myFunction() {}",
+            "d.)  create myFunction() {}"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What will the following code return: Boolean(10 > 9)?",
+        answer: "a.)  true",
+        options: [
+            "a.)  true",
+            "b.)  false",
+            "c.)  NaN",
+            "d.)  null"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "a.)  alert('Hello World');",
+        options: [
+            "a.)  alert('Hello World');",
+            "b.)  msg('Hello World');",
+            "c.)  alertBox('Hello World');",
+            "d.)  msgBox('Hello World');"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is a correct way to declare a variable in JavaScript?",
+        answer: "a.)  let myVar;",
+        options: [
+            "a.)  let myVar;",
+            "b.)  variable myVar;",
+            "c.)  v myVar;",
+            "d.)  varr myVar;"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How can you detect the client's browser name?",
+        answer: "a.)  navigator.appName",
+        options: [
+            "a.)  navigator.appName",
+            "b.)  browser.name",
+            "c.)  client.navName",
+            "d.)  browser.appName"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which method finds the HTML element by ID?",
+        answer: "a.)  document.getElementById()",
+        options: [
+            "a.)  document.getElementById()",
+            "b.)  document.getElementByName()",
+            "c.)  document.findId()",
+            "d.)  document.getId()"
+        ]
+    },
+    {
+        numb: 7,
+        question: "How do you find the number with the highest value of x and y?*",
+        answer: "a.)  Math.max(x, y)",
+        options: [
+            "a.)  Math.max(x, y)",
+            "b.)  ceil(x, y)",
+            "c.)  top(x, y)",
+            "d.)  Math.ceil(x, y)"
+        ]
+    },
+    {
+        numb: 8,
+        question: "How does a while loop start?",
+        answer: "d.)  while (condition) {}",
+        options: [
+            "a.)  while (condition)",
+            "b.)  while condition",
+            "c.)  while {condition}",
+            "d.)  while (condition) {}"
+        ]
+    },
+    {
+        numb: 9,
+        question: "How can you add a single-line comment in JavaScript?",
+        answer: "a.)  // comment",
+        options: [
+            "a.)  // comment",
+            "b.)  # comment",
+            "c.)   comment  ",
+            "d.)  ** comment"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the purpose of the isNaN function?",
+        answer: "a.)  To check if a value is not a number",
+        options: [
+            "a.)  To check if a value is not a number",
+            "b.)  To check if a number is negative  ",
+            "c.)  To convert a string to a number",
+            "d.)  To check if a number is an integer  "
+        ]
+    },
+];
